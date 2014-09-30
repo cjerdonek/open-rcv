525f604a898967b2f07e53264479b5609173d2b7
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-09-29 18:47:45 -0700

    Add a BLT parser test.

--- openrcv/test/test.py
+++ openrcv/test/test.py
@@ -1,12 +1,44 @@
 
+from io import StringIO
+from textwrap import dedent
 import unittest
+from unittest import TestCase
+
+from openrcv.main import BLTParser, ContestInfo
+
 
 def run_tests():
     # TODO: discover all tests.
     unittest.main(module=__name__)
 
 
-class MainTestCase(unittest.TestCase):
+class MainTestCase(TestCase):
 
     def test(self):
         self.assertEqual(1, 1)
+
+
+class BLTParserTest(TestCase):
+
+    def test_parse_file(self):
+        blt = dedent("""\
+        4 2
+        -3
+        2 2 0
+        1 2 4 3 1 0
+        2 1 3 4 0
+        3 1 0
+        2 3 4 0
+        4 4 1 3 0
+        0
+        "Jen"
+        "Alice"
+        "Steve"
+        "Bill"
+        "My Election"
+        """)
+        parser = BLTParser()
+
+        with StringIO(blt) as f:
+            info = parser.parse_file(f)
+        self.assertEqual(type(info), ContestInfo)

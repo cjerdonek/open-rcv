554ff96a5898f4793a90437fb77308aea3c0e1f4
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-09-29 23:01:21 -0700

    Add utils.py.

--- openrcv/parsing.py
+++ openrcv/parsing.py
@@ -1,33 +1,6 @@
 
-from contextlib import contextmanager
-import timeit
-import sys
-
 from openrcv.models import ContestInfo
-
-
-# TODO: use Python's logging module.
-# TODO: move this to an utils.py.
-def log(s=None):
-    """Write to stderr."""
-    if s is None:
-        s = ""
-    print(s, file=sys.stderr)
-
-
-@contextmanager
-def time_it(task_desc):
-    """
-    A context manager for timing chunks of code and logging it.
-
-    Arguments:
-      task_desc: task description for logging purposes
-
-    """
-    start_time = timeit.default_timer()
-    yield
-    elapsed = timeit.default_timer() - start_time
-    log("elapsed (%s): %.4f seconds" % (task_desc, elapsed))
+from openrcv.utils import log, time_it
 
 
 class Parser(object):
---
+++ openrcv/utils.py
@@ -0,0 +1,28 @@
+
+from contextlib import contextmanager
+import timeit
+import sys
+
+
+# TODO: use Python's logging module.
+# TODO: move this to an utils.py.
+def log(s=None):
+    """Write to stderr."""
+    if s is None:
+        s = ""
+    print(s, file=sys.stderr)
+
+
+@contextmanager
+def time_it(task_desc):
+    """
+    A context manager for timing chunks of code and logging it.
+
+    Arguments:
+      task_desc: task description for logging purposes
+
+    """
+    start_time = timeit.default_timer()
+    yield
+    elapsed = timeit.default_timer() - start_time
+    log("elapsed (%s): %.4f seconds" % (task_desc, elapsed))

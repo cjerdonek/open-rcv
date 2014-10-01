cd910e5194406c7b8f848c97459da750b3874693
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-10-01 16:44:25 -0700

    Create BallotList class.

--- openrcv/main.py
+++ openrcv/main.py
@@ -7,6 +7,8 @@ This module houses the "highest-level" programmatic API.
 
 import sys
 
+from openrcv import models
+from openrcv.models import BallotList
 from openrcv.parsing import BLTParser
 from openrcv.utils import FILE_ENCODING
 
@@ -14,6 +16,9 @@ from openrcv.utils import FILE_ENCODING
 def do_parse(ballots_path, encoding=None):
     if encoding is None:
         encoding = FILE_ENCODING
+
+    ballots = models.random_ballot_list(range(6), 5)
+    #print(repr(ballots.ballots))
     parser = BLTParser()
     info = parser.parse_path(ballots_path)
     print(repr(info))
--- openrcv/models.py
+++ openrcv/models.py
@@ -1,4 +1,48 @@
 
+from random import randint
+
+def random_ballot_list(choices, count, max_length=None):
+    """
+    Arguments:
+      choices: a sequence of integers.
+
+    """
+    if max_length is None:
+        max_length = len(choices)
+
+    ballots = []
+    choice_count = len(choices)
+    for i in range(count):
+        ballot = []
+        for j in range(max_length):
+            choice_index = randint(0, choice_count)
+            try:
+                choice = choices[choice_index]
+            except IndexError:
+                # Then end the ballot early.
+                break
+            ballot.append(choice)
+        ballots.append(ballot)
+
+    return BallotList(ballots)
+
+
+class BallotList(object):
+
+    """
+
+    This should be used only for tests and small sets of ballots.
+    For large numbers of ballots, ballot data should be kept on the
+    file system for memory reasons.
+
+    """
+
+    def __init__(self, ballots=None):
+        if ballots is None:
+            ballots = []
+        self.ballots = ballots
+
+
 class ContestInfo(object):
 
     """

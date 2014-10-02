8f068e2b1d21dddf1eb2298259c1b8b78eb956be
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-10-02 07:14:39 -0700

    Add ballotgen.py.

--- openrcv/models.py
+++ openrcv/models.py
@@ -7,38 +7,6 @@ def to_json(obj):
     return json.dumps(obj, indent=4)
 
 
-def random_ballot_list(choices, count, max_length=None):
-    """
-    Arguments:
-      choices: a sequence of integers.
-
-    """
-    if max_length is None:
-        max_length = len(choices)
-
-    ballots = []
-    choice_count = len(choices)
-    for i in range(count):
-        ballot = []
-        for j in range(max_length):
-            choice_index = randint(0, choice_count)
-            try:
-                choice = choices[choice_index]
-            except IndexError:
-                # Then end the ballot early.
-                break
-            ballot.append(choice)
-        ballots.append(ballot)
-
-    return BallotList(ballots)
-
-
-def random_contest(candidates):
-    ballots = random_ballot_list(range(candidates), 5)
-    contest = MinimalContest(candidates, ballots)
-    return contest
-
-
 class JsonMixin(object):
 
     def to_json(self):

600513fa3f388ac878864933d44c86db547c1081
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-10-02 07:09:21 -0700

    Stub out counting module.

---
+++ openrcv/counting.py
@@ -0,0 +1,12 @@
+
+"""
+Support for counting ballots.
+
+"""
+
+from openrcv.parsing import BLTParser
+
+
+# This is currently just a test function rather than part of the API.
+def count_irv(blt_path):
+    pass

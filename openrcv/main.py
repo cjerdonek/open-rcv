92631d96a9acac10e8af98bbaa5ec2afee1ae12f
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-10-02 07:02:30 -0700

    Add code for generating test files.

--- openrcv/main.py
+++ openrcv/main.py
@@ -13,12 +13,25 @@ from openrcv.parsing import BLTParser
 from openrcv.utils import FILE_ENCODING
 
 
+def make_json_tests():
+    contests = []
+    for count in range(3, 6):
+        contest = models.random_contest(count)
+        contests.append(contest)
+
+    contests_obj = [c.__jsobj__() for c in contests]
+
+    tests_jobj = {
+        "_version": "0.1.0-alpha",
+        "contests": contests_obj
+    }
+    json = models.to_json(tests_jobj)
+
+    print(json)
+
 def do_parse(ballots_path, encoding=None):
     if encoding is None:
         encoding = FILE_ENCODING
-
-    ballots = models.random_ballot_list(range(6), 5)
-    #print(repr(ballots.ballots))
     parser = BLTParser()
     info = parser.parse_path(ballots_path)
     print(repr(info))

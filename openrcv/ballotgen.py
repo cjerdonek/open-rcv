a5916a559a8f606afb8d3dafcd8e4886478d4bc9
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-10-03 19:00:54 -0700

    Remove sandbox file.

--- openrcv/ballotgen.py
+++ openrcv/ballotgen.py
@@ -37,3 +37,20 @@ def random_contest(candidates):
     ballots = random_ballot_list(range(candidates), 5)
     contest = MinimalContest(candidates, ballots)
     return contest
+
+
+def make_json_tests():
+    contests = []
+    for count in range(3, 6):
+        contest = models.random_contest(count)
+        contests.append(contest)
+
+    contests_obj = [c.__jsobj__() for c in contests]
+
+    tests_jobj = {
+        "_version": "0.1.0-alpha",
+        "contests": contests_obj
+    }
+    json = models.to_json(tests_jobj)
+
+    print(json)

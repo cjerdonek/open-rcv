307f13adda070c96976f99321678a5272708de76
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-10-25 06:43:21 -0700

    Stub out use of argparse.

--- openrcv/scripts/rcv.py
+++ openrcv/scripts/rcv.py
@@ -4,6 +4,7 @@ The rcv command for counting ballots.
 
 """
 
+import argparse
 import logging
 import sys
 
@@ -12,13 +13,35 @@ from openrcv.scripts.main import main
 
 log = logging.getLogger(__name__)
 
+DESCRIPTION = """\
+Tally the contests specified by the file at INPUT_PATH.
+
+"""
+
+
+def create_argparser():
+    """
+    Return an ArgumentParser object.
+
+    """
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('input_path', metavar='INPUT_PATH',
+                        help="path to a contests configuration file. "
+                             "Supported file formats are JSON (*.json) and "
+                             "YAML (*.yaml or *.yml).")
+    return parser
+
 
 def run_main():
     main(run_rcv)
 
+
 def run_rcv(argv):
     if argv is None:
         argv = sys.argv
-    # TODO: use argparse.
-    blt_path = argv[1]
+    parser = create_argparser()
+    ns = parser.parse_args()  # Namespace object
+    input_path = ns.input_path
+    # TODO: read BLT path from input_path.
+    blt_path = input_path
     counting.count_irv(blt_path)

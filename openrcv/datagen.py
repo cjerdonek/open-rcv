647c98e34e6bafec101a9181e9f2868971edbf2d
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-11-27 00:59:53 -0800

    Remove rcvgen.

--- openrcv/datagen.py
+++ openrcv/datagen.py
@@ -201,3 +201,51 @@ def create_json_tests():
 
     test_file = JsonContestFile(contests, version="0.2.0-alpha")
     return test_file
+
+
+def count_contest_file(argv):
+    rule_set = "san_francisco_irv"
+    jsobj = read_json_path(TEST_INPUT_PATH)
+    contest_file = JsonContestFile.from_jsobj(jsobj)
+
+    results = JsonTestCaseFile()
+    results.version = contest_file.version
+    results.rule_set = rule_set
+    test_cases = []
+    results.test_cases = test_cases
+
+    for id_, contest in enumerate(contest_file.contests, start=1):
+        test_case = JsonTestCase()
+        test_case.id = id_
+        test_case.contest_id = contest.id
+        test_case.input.contest = contest
+
+        # Tabulate results.
+        candidates = contest.get_candidates()
+        ballot_stream = JsonBallot.to_ballot_stream(contest.ballots)
+        contest_results = count_irv_contest(ballot_stream, candidates)
+
+        # Add results to output.
+        output_rounds = test_case.output.rounds
+        for round_results in contest_results.rounds:
+            json_round = JsonRoundResults()
+            json_round.totals = round_results.totals
+            output_rounds.append(json_round)
+
+        test_cases.append(test_case)
+
+    print(results.to_json())
+    path = os.path.join(TEST_CASE_DIR, "%s.json" % rule_set)
+    write_json(results, path=path)
+
+
+def make_input_test_file(argv):
+    # target_path="sub/open-rcv-tests/contests.json"
+
+    test_file = create_json_tests()
+    stream_info = PathInfo("temp.json")
+    models.write_json(test_file.to_jsobj(), stream_info)
+
+    with stream_info.open() as f:
+        json = f.read()
+    print(json)
